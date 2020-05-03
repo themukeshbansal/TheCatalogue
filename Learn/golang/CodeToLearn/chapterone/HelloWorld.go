c7f63b4b0652dc8ba082efdf8380a776e23d4d04
mukesh.bansal <mukesh.bansal@flipkart.com>
2020-05-03 16:00:53 +0530

    Starting Go, Commiting Content Struture

---
+++ Learn/golang/CodeToLearn/chapterone/HelloWorld.go
@@ -0,0 +1,16 @@
+package chapterone
+
+// We then want to use the fmt package
+// which features a `print` function - Println
+import "fmt"
+
+// We then need to define our main function.
+// Think of this as the entry point to our Go
+// program
+func main() {
+	// within this main function, we then
+	// want to call a function within the fmt
+	// package called Println() in order to print
+	// out `Hello World`
+	fmt.Println("Hello World")
+}

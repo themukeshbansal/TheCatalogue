6ecbbfcb1bb7926e7a824604a8776ee294a6e60f
mukesh.bansal <mukesh.bansal@flipkart.com>
2020-05-04 10:00:59 +0530

    Packaging Structures

--- Learn/golang/CodeToLearn/chapterone/HelloWorld.go
+++ Learn/golang/CodeToLearn/chapterone/HelloWorld.go
@@ -4,10 +4,8 @@ package chapterone
 // which features a `print` function - Println
 import "fmt"
 
-// We then need to define our main function.
-// Think of this as the entry point to our Go
-// program
-func main() {
+// PrintHelloWorld :  prints hello world
+func PrintHelloWorld() {
 	// within this main function, we then
 	// want to call a function within the fmt
 	// package called Println() in order to print
